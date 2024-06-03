51e2d3941f3684326890e3d0229182d5f2163164
janemadsen <janemadsen815@gmail.com>
2024-06-03 09:54:57 -0600

    Clarify wording of SF Symbol challenge

--- Special Topics/ST14 - 'SF Symbol Lookup' - SwiftUI, SFSymbols, State.playground/Contents.swift
+++ Special Topics/ST14 - 'SF Symbol Lookup' - SwiftUI, SFSymbols, State.playground/Contents.swift
@@ -1,15 +1,16 @@
-//  🏔️ MTECH Code Challenge ST14: "SFSymbol Lookup"
+//  🏔️ MTECH Code Challenge ST14: "SF Symbol Lookup"
 //  Concept: Practice creating ViewModifiers to create custom reusable view designs.
 
 //  Instructions:
-    //  In this simple challenge, all you need to do is create an app in which typing a symbol name into a textfield displays that symbol, along with a set of buttons that will change its color.
+    //  In this simple challenge, all you need to do is create an app in which typing a symbol name into a textfield displays that symbol.
+    //  Furthermore, add a set of buttons that will change the color of the symbol (you may pick what colors).
         //  To find symbol names, download the SF Symbols app: 🔗https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-5.1.dmg
 
 //  Notes:
     //  To make sure the image displays correctly, you may need to add the .resizable(), .scaledToFit(), and .frame(width:) modifiers to your Image view.
 
 //  ⌺ Black Diamond Challenge:
-    //  Add a list view that searches all SFSymbol names and displays matches. You can get a list of symbol names from the SF Symbols app by displaying all symbols in a list view, selecting all, right clicking and choosing "copy names".
+    //  Add a list view that searches all SFSymbol names and displays matches to the string in the text field. You can get a list of symbol names from the SF Symbols app by displaying all symbols in a list view, selecting all, right clicking and choosing "copy names".
 
 import SwiftUI
 import PlaygroundSupport
