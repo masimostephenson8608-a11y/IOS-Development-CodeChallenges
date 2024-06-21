20b054146b8a3d0712f2e248a397e4f32980f3e5
janemadsen <janemadsen815@gmail.com>
2024-06-21 09:59:36 -0600

    Add TP21 challenge & minor tweaks to overview keynote

---
+++ Tables and Persistence/TP21 - 'Will It Divide' - Math Puzzles.playground/Contents.swift
@@ -0,0 +1,14 @@
+//  🏔️ MTECH Code Challenge TP21: "Will It Divide"
+//  Concept: Practice translating simple math into Swift code
+
+//  Instructions:
+    //  Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+//  Examples:
+    //  Input: [15,5,6,7,2,3,45,12], 3
+    //  Output: [15,5,6]
+
+//  ⌺ Black Diamond Challenge:
+    //  Create another function. This time, return the first and last n elements of the array. If the first and last n elements overlap (i.e., 3 first and 3 last in a 5 element array), return the whole array.
+
+import Foundation
