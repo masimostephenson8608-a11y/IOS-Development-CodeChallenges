307f888d2d86cf73a613314fb4dd10c6ce222c99
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-11-04 07:54:44 -0700

    Completed TP21 and the Black Diamond

--- 2 - Tables and Persistence/TP21 - 'Will It Divide' - Math Puzzles.playground/Contents.swift
+++ 2 - Tables and Persistence/TP21 - 'Will It Divide' - Math Puzzles.playground/Contents.swift
@@ -18,3 +18,38 @@
     //  The function should return true if the number n is divisible by *all* numbers in the 'divisors' array, and false if it is not divisible by any one of them.
 
 import Foundation
+
+func checkDivide(number n: Int, dividedBy x: Int, dividedByAgain y: Int) -> Bool {
+    guard x != 0 || y != 0 else { return false }
+    if n % x != 0 || n % y != 0 {
+        return false
+    } else {
+        return true
+    }
+}
+
+func checkDivideArray(number n: Int, divisors array: [Int]) -> Bool {
+    guard !array.isEmpty && !array.contains(0) else { return false }
+    var bools: [Bool] = []
+    for number in array {
+        if n % number != 0 {
+            bools.append(false)
+        } else {
+            bools.append(true)
+        }
+    }
+    if bools.contains(false) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+print(checkDivide(number: 10, dividedBy: 2, dividedByAgain: 5))
+print(checkDivide(number: 27, dividedBy: 2, dividedByAgain: 9))
+
+print(checkDivideArray(number: 15, divisors: [1, 5, 3]))
+print(checkDivideArray(number: 27, divisors: [9, 3]))
+print(checkDivideArray(number: 45, divisors: [1, 5, 9, 15]))
+print(checkDivideArray(number: 21, divisors: [2, 5, 6, 7, 3]))
