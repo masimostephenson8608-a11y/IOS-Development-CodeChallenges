4c8e6fcd176fcacd69223e129f47fa7a414800bf
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-11-10 07:46:41 -0700

    Completed TP25

--- 2 - Tables and Persistence/TP25 - 'Rock Paper Scissors 2.0' - RNG.playground/Contents.swift
+++ 2 - Tables and Persistence/TP25 - 'Rock Paper Scissors 2.0' - RNG.playground/Contents.swift
@@ -17,3 +17,43 @@
     //  If more than two moves are input, the winners are the players who voted for the most popular move.
 
 import Foundation
+
+
+enum RockPaperScissors {
+    case rock
+    case paper
+    case scissors
+}
+
+func rockPaperScissors(player1move: RockPaperScissors) -> String {
+    var string: String = "I chose \(player1move), "
+    var randomNum = Int.random(in: 1...3)
+    var compMove: RockPaperScissors
+    switch randomNum {
+    case 1:
+        compMove = .rock
+    case 2:
+        compMove = .paper
+    case 3:
+        compMove = .scissors
+    default:
+        compMove = .rock
+    }
+    switch player1move {
+    case .paper:
+        if compMove == .paper { string += "Its a tie!" }
+        if compMove == .rock { string += "Player 1 Wins!" }
+        if compMove == .scissors { string += "Player 2 Wins!" }
+    case .rock:
+        if compMove == .rock { string += "Its a tie!" }
+        if compMove == .scissors { string += "Player 1 Wins!" }
+        if compMove == .paper { string += "Player 2 Wins!" }
+    case .scissors:
+        if compMove == .scissors { string += "Its a tie!" }
+        if compMove == .paper { string += "Player 1 Wins!" }
+        if compMove == .rock { string += "Player 2 Wins!" }
+    }
+    return string
+}
+
+print(rockPaperScissors(player1move: .paper))
