b208f7068216025540cd5840f46ec434340561db
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-11-24 09:15:28 -0700

    Did ND07 and the Black Diamond

--- 3 - Networking and Data Storage/ND07 - 'Difference Sums' - Arrays.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND07 - 'Difference Sums' - Arrays.playground/Contents.swift
@@ -19,3 +19,26 @@
     //  Calculate and return the average difference as well.
 
 import Foundation
+
+func findTotalDifferences(numbers: [Int]) -> (Int, Int) {
+    guard !numbers.isEmpty else { return (0,0) }
+    var differences: [Int] = []
+    for (index, _) in numbers.enumerated() {
+        if index + 1 != numbers.count {
+            var difference = numbers[index] - numbers[index + 1]
+            if difference < 0 {
+                difference = difference * -1
+            }
+            differences.append(difference)
+        }
+    }
+    let totalDifference = differences.reduce(0) { $0 + $1 }
+    let avgDifference = totalDifference / differences.count
+    print("""
+    Total Difference: \(totalDifference)
+    Average Difference: \(avgDifference)
+    """)
+    return (totalDifference, avgDifference)
+}
+
+findTotalDifferences(numbers: [2, 1, 10])
