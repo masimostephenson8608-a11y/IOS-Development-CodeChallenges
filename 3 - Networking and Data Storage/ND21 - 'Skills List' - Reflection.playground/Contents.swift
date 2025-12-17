0bd49cee6abda258e2503565a4c94c3009a9eba7
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-12-17 09:12:39 -0700

    Finished ND21

--- 3 - Networking and Data Storage/ND21 - 'Skills List' - Reflection.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND21 - 'Skills List' - Reflection.playground/Contents.swift
@@ -13,5 +13,7 @@
 //  MARK: Student response
 /*  Write your response below.
  
- 
+ I've learned useful things like animation which helps engage the user, and I've learned things like swift data which is really useful to save data and manage it as well, allowing for an actual app build where people can save their data.
+ I feel like ive gotten better at organizing my code, connecting the data, testing functionality, building the UI, making everything more adapatable and handling errors.
+ Projects that really helped me learn were ones like the iTunes project, helped me a lot when learning how to deal with JSON and the custom decoders, which im still shaky on but I have a better understanding. The journals lab and the Project 9 helped me get better with swift data and also learning more about animation and how to implement the matchedGeometryEffect.
  */
